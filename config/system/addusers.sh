d48e0e2a9a9880999ed4cfb8185174c515063009
WYZEN <vincent.deverre@wyzengroup.com>
2022-03-10 09:52:07 +0100

    fix uid users

--- config/system/addusers.sh
+++ config/system/addusers.sh
@@ -2,7 +2,7 @@
 
 # Creation de 10 users
 
-for INDEX in {1..10}; do
-	addgroup user${INDEX} --gid 1000${INDEX}
-	adduser user${INDEX} -q --uid 1000${INDEX} --gid 1000${INDEX} --disabled-password
+for INDEX in {1..9}; do
+	addgroup user${INDEX} --gid 100${INDEX}
+	adduser user${INDEX} -q --uid 100${INDEX} --gid 100${INDEX} --disabled-password
 done;
